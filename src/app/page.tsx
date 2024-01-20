5b7a467956b6e2e3b828ce0cbffaceb5106f96f6
kodename-kian <kodename.kian@gmail.com>
2024-01-20 13:40:46 -0800

    background image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import { Button } from "@mui/material";
+import Background from "@/components/Background";
 
 export default function Home() {
-  return;
+  return(
+    <>
+      <div style={{color:'white'}}>
+        Putting stuff on top!
+      </div>
+
+      <Background />
+    </>
+  );
 }
