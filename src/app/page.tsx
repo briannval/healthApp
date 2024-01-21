4cc3def624b32f9939f25c046043ed048660f899
kodename-kian <kodename.kian@gmail.com>
2024-01-20 16:20:59 -0800

    added font and home page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,70 @@
+"use client";
+
 import { Button } from "@mui/material";
+import Grid from '@mui/material/Grid';
+import { playfair } from './ui/fonts';
 import Background from "@/components/Background";
 
+function Landing(){
+  return(
+    <>
+      <div className="container">
+        <div>
+          <p className="bigtext"> Welcome to (name)! </p>
+          <p className="regulartext"><em>(insert tagline here)</em></p>
+          
+          <div className="buttonRow">
+            <Button href="/about" variant="outlined">Learn More </Button>
+            <Button href="/auth/signup" variant="contained">Get Started </Button>
+          </div>
+        </div>
+
+        <style jsx>{`
+          .container {
+            margin: 50px;
+            background-color: rgba(0,0,0,0.3);
+            border-radius: 25px;
+            height: 550px;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+          }
+
+          .bigtext{
+            color: white;
+            font-size: 50px;
+            font-weight: 800;
+          }
+
+          .regulartext{
+            color: white;
+            font-size: 25px;
+            font-weight: 500;
+          }
+
+          .buttonRow{
+            display: flex;
+            width: 300px;
+            justify-content: space-between;
+          }
+
+        `}</style>
+      </div>
+    </>
+  );
+}
 export default function Home() {
   return(
     <>
-      <div style={{color:'white'}}>
-        Putting stuff on top!
+      <div className={playfair.className}>
+        <Grid container spacing={2} >
+          <Grid item xs={6}>
+            <Landing />
+          </Grid>
+          <Grid item>
+            (placeholder)
+          </Grid>
+        </Grid>
       </div>
 
       <Background />
---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Playfair_Display } from 'next/font/google';
+
+export const playfair = Playfair_Display({ 
+	subsets: ['latin'],
+	style: ['normal', 'italic'],
+	weight: ['500', '800'], 
+
+});
