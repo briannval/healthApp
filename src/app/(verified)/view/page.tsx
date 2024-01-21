f9018672ed469a9abbf1005a79f742f8d9bae776
briannval <brianvalentinoadhitya@gmail.com>
2024-01-21 01:47:06 -0800

    Fetched data at view endpoint

--- src/app/(verified)/view/page.tsx
+++ src/app/(verified)/view/page.tsx
@@ -1,11 +1,54 @@
 "use client";
 import AuthNavbar from "@/components/AuthNavbar";
+import OutlinedCard from "@/components/Card";
+import { Container } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export interface Log {
+  _id: string;
+  date: string;
+  feeling: string;
+  sleep: string;
+  description: string;
+  exercise: string;
+  userId: string;
+}
 
 const View = () => {
+  let datalogs: Log[];
+
+  const getLogs = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/api/logs/view");
+      datalogs = res.data.message;
+      console.log(datalogs);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getLogs();
+  });
+
   return (
     <>
       <AuthNavbar />
-      <h1>View</h1>
+      <Container maxWidth="md" sx={{ marginY: 5 }}>
+        <Grid container spacing={10}>
+          <Grid item xs={4}>
+            <OutlinedCard />
+          </Grid>
+          <Grid item xs={4}>
+            <OutlinedCard />
+          </Grid>
+          <Grid item xs={4}>
+            <OutlinedCard />
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
